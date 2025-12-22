1fba217add5596ded79e985545e958b308c47a1e
jiteshmunda <jitesh.munda@piquota.com>
2025-12-22 17:14:06 +0530

    storage service Update

--- src/services/storage/index.js
+++ src/services/storage/index.js
@@ -1,7 +1,18 @@
 const provider = process.env.STORAGE_PROVIDER;
 
-if (provider === "gcp") {
-  module.exports = require("./gcp.storage");
-} else {
-  module.exports = require("./do.storage");
+switch (provider) {
+  case "gcp":
+    module.exports = require("./gcp.storage");
+    break;
+
+  case "digitalocean":
+    module.exports = require("./do.storage");
+    break;
+
+  case "vultr":
+    module.exports = require("./vultr.storage");
+    break;
+
+  default:
+    throw new Error("Invalid STORAGE_PROVIDER");
 }
