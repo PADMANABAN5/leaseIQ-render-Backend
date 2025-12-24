ec9d02cd0772921be77f4f71bfb71f4decf5d8df
jiteshmunda <jitesh.munda@piquota.com>
2025-12-24 14:12:27 +0530

    auth route updated: signup

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { login } = require("../controllers/auth.controller");
+const { login, signup } = require("../controllers/auth.controller");
 
 router.post("/login", login);
+router.post("/signup", signup);
 
 module.exports = router;
